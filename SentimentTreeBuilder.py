f51bc1a7c891b9424b59c0b451dc1f7520e77272
DavidBalash <dgbalash@gmail.com>
2016-12-14 20:32:55 -0500

    Added file input and file output.

--- SentimentTreeBuilder.py
+++ SentimentTreeBuilder.py
@@ -23,45 +23,65 @@ import re
 
 from nltk.corpus import sentiwordnet as swn
 from nltk.corpus import wordnet as wn
+from string import punctuation
 
+punctuation_to_remove = punctuation.replace("-", "")
+punctuation_to_remove = punctuation_to_remove.replace("'", "")
+def strip_punctuation(s):
+    return ''.join(c for c in s if c not in punctuation_to_remove)
 
-line = "(SBARQ (WHNP (WP Best))(SQ (AUX sad)(NP (NNP happy))(VP (VB bad)(PP (IN superb))))(. ?))"
+#line = "1 |BT| (ROOT (S (NP (DT The) (NNS performances)) (VP (VBP are) (NP (DT an) (JJ absolute) (NN joy))) (. .))) |ET|"
 
-words = re.sub(r'\(\S*', '', line)
-words = re.sub(r'\)', '', words)
-print(line)
+# Open the sentitree.txt file for writing
+sentitree = open('sentitree.txt', 'w')
 
-# Split on whitespace
-wordList = words.split()
+# Open the data.txt file for reading
+data_text_file = open('data.txt', 'r')
 
-for word in wordList:
-    positive = 0
-    negative = 0
-    neutral = 0
-    count = 0
+# Load the data.txt lines into a list
+data_text = data_text_file.readlines()
 
-    # For each sentiment in the synset add to the positive, negative, or neutral counts
-    for syn in wn.synsets(word):
-        count = count + 1
-        senti_synset = swn.senti_synset(syn.name())
-        positive = positive + senti_synset.pos_score()
-        negative = negative + senti_synset.neg_score()
-        neutral = neutral + senti_synset.obj_score()
+for line in data_text:
+    words = re.sub(r'\S*\s*\|BT\|', '', line)
+    words = re.sub(r'\|ET\|', '', words)
+    words = re.sub(r'\(\S*', '', words)
+    words = re.sub(r'\)', '', words)
+    words = strip_punctuation(words)
 
-    # if the count is greater than zero then normalize the values
-    if count > 0:
-        positive = positive / count
-        negative = negative / count
-        neutral = neutral / count
+    # Split on whitespace
+    wordList = words.split()
 
-    # Replace the word with the positive or negative value
-    regex = r'' + re.escape(word)
-    if ((count > 0) & (positive > negative) & (positive > neutral)):
-        line = re.sub(regex, 'P', line)
-    elif ((count > 0) & (negative > positive) & (negative > neutral)):
-        line = re.sub(regex, 'N', line)
-    else:
-        line = re.sub(regex, 'U', line)
+    # sort the word list by length descending, so that the longer words get replaced first
+    # this keeps smaller words contained within larger words from being replaced
+    wordList.sort(key=len, reverse=True)
 
+    for word in wordList:
+        positive = 0
+        negative = 0
+        neutral = 0
+        count = 0
 
-print(line)
+        # For each sentiment in the synset add to the positive, negative, or neutral counts
+        for syn in wn.synsets(word):
+            count = count + 1
+            senti_synset = swn.senti_synset(syn.name())
+            positive = positive + senti_synset.pos_score()
+            negative = negative + senti_synset.neg_score()
+            neutral = neutral + senti_synset.obj_score()
+
+        # if the count is greater than zero then normalize the values
+        if count > 0:
+            positive = positive / count
+            negative = negative / count
+            neutral = neutral / count
+
+        # Replace the word with the positive or negative value
+        regex = r'' + re.escape(word)
+        if ((count > 0) & (positive > negative) & (positive > neutral)):
+            line = re.sub(regex, 'P', line)
+        elif ((count > 0) & (negative > positive) & (negative > neutral)):
+            line = re.sub(regex, 'N', line)
+        else:
+            line = re.sub(regex, 'U', line)
+
+    sentitree.write(line)
